f7b627336dffe69c718ea2ca9c73b76cd96d4f1a
Sangam <100322380+sangT220222@users.noreply.github.com>
2024-06-07 11:45:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -20,8 +20,19 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', function (req, res) {
+  //retrieving IP address
+  var ip_address = req.connection.remoteAddress;
+  if(ip_address.includes(":ffff:")){ //as JS gives IPv6 address, below gives IPv4 addrees
+    ip_address = ip_address.split(":").pop();
+  }
+  //retrieving Preferred language of browser
+  const language_header = req.headers['accept-language'];
+
+  //retrieving software
+  const software_val = req.headers['user-agent'];
+  res.json({ ipaddress: ip_address, language : language_header, software: software_val });
+
 });
 
 // listen for requests :)
